c5f3e988ce588acd85db6e1856156e1a6446d5a1
Shivraj <shivrajganacharya1@gmail.com>
2019-08-10 21:22:10 +0530

    Message 1

---
+++ New File Bowser Project/Assets/ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+	pulic void SetText(string text)
+	{
+		Text txt = transform.Find("Text").GetComponent<Text>();	
+		txt.text = text;
+	}
+}
